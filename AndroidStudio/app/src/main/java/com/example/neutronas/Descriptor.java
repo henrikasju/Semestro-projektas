0ce68df763f057881acc1811aa4c5701bf1a13b8
Rimvydas Neverauskas <goldenpkr@yahoo.com>
2019-03-19 11:12:01 +0200

    Removed Cloud Vision OpenCV implementation Protoype that recognizes rectangles in real time

---
+++ AndroidStudio/app/src/main/java/com/example/neutronas/Descriptor.java
@@ -0,0 +1,10 @@
+package com.example.neutronas;
+
+import org.opencv.*;
+import org.opencv.android.OpenCVLoader;
+
+import java.io.File;
+
+public class Descriptor {
+    File imgFile = new  File("E:\\KTU\\2018-2019\\Projekto-Options");
+}

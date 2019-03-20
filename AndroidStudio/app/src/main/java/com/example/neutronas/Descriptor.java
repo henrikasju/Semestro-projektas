1f28aa4a4998d34173c4539a9167019074b9e5d4
Rimvydas Neverauskas <goldenpkr@yahoo.com>
2019-03-20 13:15:31 +0200

    Demo application that is able to find keypoints from a stored picture and find similarities from realtime camera frames using OpenCVs ORB.

--- AndroidStudio/app/src/main/java/com/example/neutronas/Descriptor.java
+++ AndroidStudio/app/src/main/java/com/example/neutronas/Descriptor.java
@@ -1,10 +1,36 @@
 package com.example.neutronas;
 
-import org.opencv.*;
-import org.opencv.android.OpenCVLoader;
+import android.os.Environment;
 
-import java.io.File;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class Descriptor {
-    File imgFile = new  File("E:\\KTU\\2018-2019\\Projekto-Options");
+
+    private static FeatureDetector detector = FeatureDetector.create(FeatureDetector.SIFT);
+
+    public static void detectKeyPoints(String imgName)
+    {
+        Mat dstImg = turnGrey(imgName);
+
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        detector.detect(dstImg, keyPoints);
+        Mat dstImg2 = new Mat();
+        Features2d.drawKeypoints(dstImg, keyPoints, dstImg2);
+        Imgcodecs.imwrite(Environment.getExternalStorageDirectory()+"/Neutronas/"+imgName+"_KPs.png", dstImg2);
+    }
+
+    public static Mat turnGrey(String imgName)
+    {
+        String pathName = Environment.getExternalStorageDirectory()+"/"+imgName+".png";
+        Mat srcImg = Imgcodecs.imread(pathName, Imgcodecs.IMREAD_UNCHANGED);
+        Mat dstImg = srcImg.clone();
+        Imgproc.cvtColor(srcImg, dstImg, Imgproc.COLOR_RGBA2GRAY);
+        Imgcodecs.imwrite(Environment.getExternalStorageDirectory()+"/Neutronas/"+imgName+"_Grey.png", dstImg);
+        return dstImg;
+    }
 }
--- AndroidStudio/app/src/main/java/com/example/neutronas/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/neutronas/MainActivity.java
@@ -1,161 +1,88 @@
 package com.example.neutronas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
+import org.opencv.core.DMatch;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
-import static org.opencv.imgproc.Imgproc.Canny;
-
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    //view holder
-    CameraBridgeViewBase cameraBridgeViewBase;
 
-    //camera listener callback
-    BaseLoaderCallback baseLoaderCallback;
+    static {
+        System.loadLibrary("opencv_java");
+        System.loadLibrary("nonfree");
+    }
 
-    //image holder
-    Mat bwIMG, hsvIMG, lrrIMG, urrIMG, dsIMG, usIMG, cIMG, hovIMG, pattern;
-    MatOfPoint2f approxCurve;
+    private int w, h;
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Scalar RED = new Scalar(255, 0, 0);
+    private Scalar GREEN = new Scalar(0, 255, 0);
+    private FeatureDetector detector;
+    private DescriptorExtractor descriptor;
+    private DescriptorMatcher matcher;
+    private Mat descriptors2, descriptors1, img1;
+    private MatOfKeyPoint keypoints1, keypoints2;
 
-    int threshold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("Logger", "called onCreate");
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
-        //initialize treshold
-        threshold = 100;
-
-        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraViewer);
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
-
-        //create camera listener callback
-        baseLoaderCallback = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status) {
-                    case LoaderCallbackInterface.SUCCESS:
-                        Log.v("aashari-log", "Loader interface success");
-                        bwIMG = new Mat();
-                        dsIMG = new Mat();
-                        hsvIMG = new Mat();
-                        lrrIMG = new Mat();
-                        urrIMG = new Mat();
-                        usIMG = new Mat();
-                        cIMG = new Mat();
-                        hovIMG = new Mat();
-                        approxCurve = new MatOfPoint2f();
-                        cameraBridgeViewBase.enableView();
-                        break;
-                    default:
-                        super.onManagerConnected(status);
-                        break;
-                }
-            }
-        };
-
+        mOpenCvCameraView = findViewById(R.id.cameraViewer);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        //Descriptor.turnGrey("Simple1");
+        //Descriptor.saveKeyPointImg("Simple2");
+        //Descriptor.detectKeyPoints("Simple3");
+        //Descriptor.detectKeyPoints("Simple4");
+        //Descriptor.turnGrey("Simple5");
+        w = width;
+        h = height;
     }
 
     @Override
     public void onCameraViewStopped() {
-
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-        Mat gray = inputFrame.gray();
-        Mat dst = inputFrame.rgba();
-
-        Imgproc.pyrDown(gray, dsIMG, new Size(gray.cols() / 2, gray.rows() / 2));
-        Imgproc.pyrUp(dsIMG, usIMG, gray.size());
-
-        Canny(usIMG, bwIMG, 0, threshold);
-
-        Imgproc.dilate(bwIMG, bwIMG, new Mat(), new Point(-1, 1), 1);
-
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-
-        cIMG = bwIMG.clone();
-
-        Imgproc.findContours(cIMG, contours, hovIMG, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-
-        for (MatOfPoint cnt : contours) {
-
-            MatOfPoint2f curve = new MatOfPoint2f(cnt.toArray());
-
-            Imgproc.approxPolyDP(curve, approxCurve, 0.02 * Imgproc.arcLength(curve, true), true);
-
-            int numberVertices = (int) approxCurve.total();
-
-            double contourArea = Imgproc.contourArea(cnt);
-
-            if (Math.abs(contourArea) < 100) {
-                continue;
-            }
-
-            //Rectangle detected
-            if (numberVertices >= 4 && numberVertices <= 6) {
-
-                List<Double> cos = new ArrayList<>();
-
-                for (int j = 2; j < numberVertices + 1; j++) {
-                    cos.add(angle(approxCurve.toArray()[j % numberVertices], approxCurve.toArray()[j - 2], approxCurve.toArray()[j - 1]));
-                }
-
-                Collections.sort(cos);
-
-                double mincos = cos.get(0);
-                double maxcos = cos.get(cos.size() - 1);
-
-                if (numberVertices == 4 && mincos >= -0.1 && maxcos <= 0.3) {
-                    setLabel(dst, "X", cnt);
-                }
-
-            }
-
-
-        }
-
-        return dst;
-
+        return recognize(inputFrame.rgba());
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
         }
     }
 
@@ -163,74 +90,108 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
-            Toast.makeText(getApplicationContext(), "There is a problem", Toast.LENGTH_SHORT).show();
+            Log.d("Logger", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
         } else {
-            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+            Log.d("Logger", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            System.loadLibrary("nonfree");
         }
     }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("Logger", "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    try {
+                        initializeOpenCVDependencies("Simple3");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
         }
     }
 
-    private static double angle(Point pt1, Point pt2, Point pt0) {
-        double dx1 = pt1.x - pt0.x;
-        double dy1 = pt1.y - pt0.y;
-        double dx2 = pt2.x - pt0.x;
-        double dy2 = pt2.y - pt0.y;
-        return (dx1 * dx2 + dy1 * dy2) / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+    public void initializeOpenCVDependencies(String imgName) throws IOException {
+        mOpenCvCameraView.enableView();
+        detector = FeatureDetector.create(FeatureDetector.ORB);
+        descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+        matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+        img1 = new Mat();
+        String pathName = Environment.getExternalStorageDirectory()+"/"+imgName+".png";
+        Mat srcImg = Imgcodecs.imread(pathName, Imgcodecs.IMREAD_UNCHANGED);
+        img1 = srcImg.clone();
+        Imgproc.cvtColor(srcImg, img1, Imgproc.COLOR_RGBA2GRAY);
+        Imgcodecs.imwrite(Environment.getExternalStorageDirectory()+"/Neutronas/"+imgName+"_KPs.png", img1);
+
+        img1.convertTo(img1, 0); //converting the image to match with the type of the cameras image
+        descriptors1 = new Mat();
+        keypoints1 = new MatOfKeyPoint();
+        detector.detect(img1, keypoints1);
+        descriptor.compute(img1, keypoints1, descriptors1);
     }
 
-    private void setLabel(Mat im, String label, MatOfPoint contour) {
-        int fontface = Core.CMP_LT;
-        double scale = 3;//0.4;
-        int thickness = 3;//1;
-        int[] baseline = new int[1];
-        Size text = Imgproc.getTextSize(label, fontface, scale, thickness, baseline);
-        Rect r = Imgproc.boundingRect(contour);
-        Point pt = new Point(r.x + ((r.width - text.width) / 2),r.y + ((r.height + text.height) / 2));
-        Imgproc.putText(im, label, pt, fontface, scale, new Scalar(255, 0, 0), thickness);
-    }
-    /*
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public Mat recognize(Mat aInputFrame) {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Imgproc.cvtColor(aInputFrame, aInputFrame, Imgproc.COLOR_RGB2GRAY);
+        descriptors2 = new Mat();
+        keypoints2 = new MatOfKeyPoint();
+        detector.detect(aInputFrame, keypoints2);
+        descriptor.compute(aInputFrame, keypoints2, descriptors2);
 
-        final Button captureButton = findViewById(R.id.CaptureButton);
-
-        captureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Camera button pressed!");
-                dispatchTakePictureIntent();
-
-                captureButton.setVisibility(View.INVISIBLE);
-            }
-
-        });
-    }
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        // Matching
+        MatOfDMatch matches = new MatOfDMatch();
+        if (img1.type() == aInputFrame.type()) {
+            matcher.match(descriptors1, descriptors2, matches);
+        } else {
+            return aInputFrame;
         }
-    }
+        List<DMatch> matchesList = matches.toList();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView imageView = findViewById(R.id.picturePreview);
-            imageView.setImageBitmap(imageBitmap);
-            imageView.setVisibility(View.VISIBLE);
+        Double max_dist = 0.0;
+        Double min_dist = 100.0;
+
+        for (int i = 0; i < matchesList.size(); i++) {
+            Double dist = (double) matchesList.get(i).distance;
+            if (dist < min_dist)
+                min_dist = dist;
+            if (dist > max_dist)
+                max_dist = dist;
         }
-    }*/
+
+        LinkedList<DMatch> good_matches = new LinkedList<>();
+        for (int i = 0; i < matchesList.size(); i++) {
+            if (matchesList.get(i).distance <= (1.5 * min_dist))
+                good_matches.addLast(matchesList.get(i));
+        }
+
+        MatOfDMatch goodMatches = new MatOfDMatch();
+        goodMatches.fromList(good_matches);
+        Mat outputImg = new Mat();
+        MatOfByte drawnMatches = new MatOfByte();
+        if (aInputFrame.empty() || aInputFrame.cols() < 1 || aInputFrame.rows() < 1) {
+            return aInputFrame;
+        }
+        Features2d.drawMatches(img1, keypoints1, aInputFrame, keypoints2, goodMatches, outputImg, GREEN, RED, drawnMatches, Features2d.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS);
+        Mat dstOutputImg = new Mat();
+        Imgproc.resize(outputImg, dstOutputImg, aInputFrame.size());
+        return dstOutputImg;
+    }
 }

50a25d061b8ae75f4a6f1f09ca47535690defe84
henrikasju <47780878+henrikasju@users.noreply.github.com>
2019-04-27 20:15:54 +0300

    GUI upload as a new project
    App new project with GUI

---
+++ GUI Project/App/app/src/main/java/com/example/app/Gallery.java
@@ -0,0 +1,28 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Gallery extends AppCompatActivity {
+
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        backButton = (Button) findViewById(R.id.back_button_gallery);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(Gallery.this, MainActivity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+    }
+}
